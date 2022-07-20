0953dcced722f251e5aa1c1868d9c5ba8edfca75
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-20 17:19:47 -0400

    post model

---
+++ models/Post.js
@@ -0,0 +1,86 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'title',
+                    'cover_url',
+                    'post_body',
+                    'view_count',
+                    'created_at',
+                    [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+                ],
+                include: [
+                    {
+                        model: models.Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                        include: {
+                            model: models.User,
+                            attributes: ['username']
+                        }
+                    }
+                ]
+            });
+        });
+    }
+}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        cover_url: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isURL: true
+            }
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: false
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }, view_count: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            default: 0
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
