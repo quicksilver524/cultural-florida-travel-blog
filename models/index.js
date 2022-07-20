fe8dd5ad98f58ca794a3bac7d3538c474ebb54e4
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-20 17:18:29 -0400

    comment model

--- models/index.js
+++ models/index.js
@@ -1,27 +1,69 @@
-// import models 
-const User = require('./User');
+// import all models
 const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
 const Comment = require('./Comment');
 
-//create associations include foreign key and what happens to data on delete 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-//post belongs to user
 Post.belongsTo(User, {
-    foreignKey: "user_id",
-    onDelete: 'CASCADE'
-})
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
-//comments belong to a post 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
 Comment.belongsTo(Post, {
-    foreignKey: "post_id",
-      
-})
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
 
-//post has many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
 Post.hasMany(Comment, {
-    foreignKey: "post_id",
-   
+    foreignKey: 'post_id'
+});
 
-})
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Post, Vote, Comment };
