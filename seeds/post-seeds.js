e1de14972ff0416dbe4497adad9606c5e9478ae6
Carly Donais <carlydonais@Carlys-MBP.lan>
2022-07-20 19:56:34 -0400

    testing seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,21 +2,19 @@ const {Post} = require('../models');
 
 const postdata = [
     {
-        title: 'drink',
-        cover_url: 'https://images.unsplash.com/photo-1634153504290-8f258fe41701?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2225&q=80',
-        post_body: 'It’s time to indulge, and throw yourself into the world of culinary photography — with shots of elaborate dinner parties, decadent pastries and more.',
+        title: 'St. Augustine Fountain of Youth',
+        cover_url: 'https://www.floridashistoriccoast.com/things-to-do/history/fountain-youth/',
+        post_body: ' Beautiful historic place, loved visiting, left looking 5 years younger!',
         view_count: 10,
         user_id: 1
-    }, {
-        title: 'eat',
-        cover_url: 'https://images.unsplash.com/photo-1657802021135-13fbd579fbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2448&q=80',
-        post_body: 'As Sulimaniyah, Riyadh Saudi Arabia\n' +
-            'Published 6 days ago\n' +
-            'Apple, iPhone 6\n' +
-            'Free to use under the Unsplash License',
-        view_count: 16,
-        user_id: 2
-    },
+    }, 
+        {
+            title: 'Reef scuba diving',
+            cover_url: 'https://www.tripadvisor.com/Attraction_Review-g34344-d142786-Reviews-Molasses_Reef-Key_Largo_Florida_Keys_Florida.html',
+            post_body: "Easily accessible, Molasses Reef Sanctuary Preservation Area is the most heavily visited reef in the Upper Keys – perhaps the world – for diving. Molasses is famous for its clear water, many fish, and numerous boulder corals. It is a classic outer reef with a well-defined spur and groove system of coral development. At the central portion of Molasses, offshore of the light, are a large ship's winch and historic Spanish anchor.",
+            view_count: 16,
+            user_id: 2
+        },    
 
 ];
 
