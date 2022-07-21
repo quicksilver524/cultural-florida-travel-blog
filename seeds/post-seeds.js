e7d6063d15e32102686ea4e4b524ff83084d8650
quicksilver524 <quicksilver524@gmail.com>
2022-07-20 20:55:04 -0400

    post seeds update

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -31,18 +31,6 @@ const postdata = [
     view_count: 16,
     user_id: 2,
   },
-  {
-    title: "eat",
-    cover_url:
-      "https://images.unsplash.com/photo-1657802021135-13fbd579fbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2448&q=80",
-    post_body:
-      "As Sulimaniyah, Riyadh Saudi Arabia\n" +
-      "Published 6 days ago\n" +
-      "Apple, iPhone 6\n" +
-      "Free to use under the Unsplash License",
-    view_count: 16,
-    user_id: 2,
-  },
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
