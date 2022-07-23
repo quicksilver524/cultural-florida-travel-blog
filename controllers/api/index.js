1309c03973bc0d76a5e141af9e6f7ad6b55880ad
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-23 09:46:21 -0400

    user-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
 const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
+
 
 router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
