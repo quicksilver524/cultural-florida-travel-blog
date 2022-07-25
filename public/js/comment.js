19f4d57b4be419480e0b1b5335c47cf19ee38a0c
Lindsey Sherrill <lindsey350@aol.com>
2022-07-25 19:19:40 -0400

    index routes added

--- public/js/comment.js
+++ public/js/comment.js
@@ -7,28 +7,28 @@ async function commentFormHandler(event) {
       window.location.toString().split('/').length - 1
     ];
   
-    console.log(comment_text, post_id);
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          post_id,
+          comment_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
   }
   
   document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
 
 
   //14.3.5 Replace the console.log() statement with the following code:
-//   if (comment_text) {
-//     const response = await fetch('/api/comments', {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         post_id,
-//         comment_text
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
   
-//     if (response.ok) {
-//       document.location.reload();
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
