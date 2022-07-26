565525c0ccb9b93e2b25663297d649436dea6f21
Lindsey Sherrill <lindsey350@aol.com>
2022-07-25 21:05:30 -0400

    routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth =  require('../utils/auth')
 
-router.get('/', withAuth, (req, res)=>{ 
-    Post.findAll({
+router.get('/', (req, res)=>{ 
+  // router.get('/', withAuth, (req, res)=>{ 
+  Post.findAll({
         where: {
             user_id: req.session.user_id
         }
