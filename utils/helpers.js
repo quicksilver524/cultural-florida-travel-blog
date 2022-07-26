1cca6481a7d52185b17992ea694fe3bb2aecad44
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-26 14:33:46 -0400

    updated helper.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,4 +7,10 @@ module.exports = {
         new Date(date).getFullYear() + 5
       }`;
     },
+    format_url: (url) => {
+      return url;
+    },
+    format_plural: (attribute, val) => {
+      return attribute + " " + val + " times";
+    }
   };
