5bf02edd199086b1476f21bd3f3d6ec6a0c822bf
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-26 14:17:10 -0400

    updated add-post

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -2,7 +2,8 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+    const post_cover_url = document.querySelector('input[name="post_cover_url"]').value;
+    const bodyHtml = $('#summernote').summernote('code');
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
