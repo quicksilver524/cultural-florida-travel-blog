aff01f73e75224a0fc80e490a727ac9ed678e453
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-27 10:12:13 -0400

    fixed login

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event) {
   const password = document.querySelector("#password-login").value.trim();
 
   // if (email && password) {
-  const response = await fetch("/login", {
+  const response = await fetch("/api/users/login", {
     method: "post",
     body: JSON.stringify({
       email: email,
